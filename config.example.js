5efe77d7dbfeed2c7a66f491f3bb2e0059843d05
mxve <68632137+mxve@users.noreply.github.com>
2022-03-23 16:31:48 +0100

    add analytics support to config & footer

--- config.example.js
+++ config.example.js
@@ -5,7 +5,8 @@ let config = {
     api_query_interval: 5,
     // listen port
     port: 1998,
-    preview_generator_heartbeat_url: ''
+    preview_generator_heartbeat_url: '',
+    analytics: '<script>console.log("analytics")</script>'
 }
 
 module.exports = config
