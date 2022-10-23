9ecdd65aac009c086adf1f93ae56ae2f419ff31e
mxve <68632137+mxve@users.noreply.github.com>
2022-10-23 08:07:31 +0200

    add db

---
+++ api/db.js
@@ -0,0 +1,105 @@
+const mongoose = require("mongoose")
+
+const serverSchema = new mongoose.Schema({
+    identifier: String,
+    ip: String,
+    port: Number,
+    platform: String,
+    game: String,
+    hostname: String,
+    hostname_display: String,
+    map: String,
+    map_display: String,
+    gametype: String,
+    gametype_display: String,
+    clients_max: Number,
+    clients: Number,
+    bots: Number,
+    players: Array,
+    hardcore: Boolean,
+    password: Boolean,
+    round: Number,
+    voice: Boolean,
+    description: String,
+    cod_info: String,
+    version: String,
+    last_seen: Date,
+    country_code: String,
+    country: String
+})
+
+const Server = mongoose.model('Server', serverSchema)
+
+const getServer = async (identifier) => {
+    const server = await Server.findOne({ identifier })
+    return server
+}
+
+const getServerByIp = async (ip, port) => {
+    const server = await Server.findOne({ ip, port })
+    return server
+}
+
+const getServers = async () => {
+    const servers = await Server.find({})
+    return servers
+}
+
+const getServersByGame = async (game) => {
+    const servers = await Server.find({ game })
+    return servers
+}
+
+const getServersByPlatform = async (platform) => {
+    const servers = await Server.find({ platform })
+    return servers
+}
+
+const insertServer = async (server) => {
+    const newServer = new Server(server)
+    await newServer.save()
+}
+
+const updateServer = async (server) => {
+    await Server.updateOne({ identifier: server.identifier }, server)
+}
+
+const updateOrInsertServer = async (server) => {
+    const existingServer = await getServer(server.identifier)
+    if (existingServer) {
+        await updateServer(server)
+    } else {
+        await insertServer(server)
+    }
+}
+
+const deleteServer = async (identifier) => {
+    await Server.deleteOne({ identifier })
+}
+
+const deleteServerByIp = async (ip, port) => {
+    await Server.deleteOne({ ip, port })
+}
+
+const connect = async (uri) => {
+    await mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+}
+
+module.exports = {
+    connect,
+    serverSchema,
+    Server,
+    getServer,
+    getServerByIp,
+    getServers,
+    getServersByGame,
+    getServersByPlatform,
+    insertServer,
+    updateServer,
+    updateOrInsertServer,
+    deleteServer,
+    deleteServerByIp
+}
