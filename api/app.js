c3821a2e98fa64041b2cc0853e1508884860be16
mxve <68632137+mxve@users.noreply.github.com>
2022-10-23 08:08:43 +0200

    push servers to db TODO: return matching object from platform getter

--- api/app.js
+++ api/app.js
@@ -1,11 +1,13 @@
 const global_config = require('../config.json')
+const config = require('./config.json')
 
 const express = require('express')
 const { ToadScheduler, SimpleIntervalJob, Task } = require('toad-scheduler')
 
 const plutonium = require('./plutonium.js')
 const xlabs = require('./xlabs.js')
-
+const db = require('./db.js')
+db.connect(config.db.uri)
 const scheduler = new ToadScheduler()
 
 let servers = {
@@ -14,10 +16,87 @@ let servers = {
 }
 
 const get_plutonium_servers = new Task('get_plutonium_servers', async () => {
-    servers.plutonium = await plutonium.getServers()
+    let _servers = await plutonium.getServers()
+
+    for (server of _servers.servers.all) {
+        // map server to database server
+        // TODO: do in plutonium.js
+        let _server = {}
+        _server.identifier = server.identifier
+        _server.ip = server.ip
+        _server.port = server.port
+        _server.platform = 'plutonium'
+        _server.game = server.game
+        _server.hostname = server.hostname
+        _server.hostname_display = server.hostnameDisplay
+        _server.map = server.map
+        _server.map_display = server.mapDisplay
+        _server.gametype = server.gametype
+        _server.gametype_display = server.gametypeDisplay
+        _server.clients_max = server.maxplayers
+        _server.clients = server.players.length - server.bots
+        _server.bots = server.bots
+        _server.players = server.players
+        _server.hardcore = server.hardcore
+        _server.password = server.password
+        _server.round = server.round
+        _server.voice = server.voice
+        _server.description = server.description
+        _server.cod_info = server.codInfo
+        _server.version = server.revision
+        _server.last_seen = new Date()
+        _server.country_code = server.country
+        _server.country = server.countryDisplay
+
+        // add server to database
+        await db.updateOrInsertServer(_server)
+    }
+
+    servers.plutonium = _servers
 })
 const get_xlabs_servers = new Task('get_xlabs_servers', async () => {
-    servers.xlabs = await xlabs.getServers()
+    let _servers = await xlabs.getServers()
+
+    for (server of _servers.servers.all) {
+        // map server to database server
+        // TODO: do in plutonium.js
+        let _server = {}
+        _server.identifier = server.identifier
+        _server.ip = server.ip
+        _server.port = server.port
+        _server.platform = 'xlabs'
+        _server.game = server.game
+        _server.hostname = server.hostname
+        _server.hostname_display = server.hostnameDisplay
+        _server.map = server.map
+        _server.map_display = server.mapDisplay
+        _server.gametype = server.gametype
+        _server.gametype_display = server.gametypeDisplay
+        _server.clients_max = server.maxplayers
+        if (server.game == 'iw4x') {
+            _server.clients = server.clients
+        } else {
+            _server.clients = server.players.length - server.bots
+        }
+        _server.bots = server.bots
+        _server.players = server.players
+        _server.hardcore = server.hardcore
+        _server.password = server.password
+        _server.round = server.round
+        _server.voice = server.voice
+        _server.aimassist = server.aimassist
+        _server.description = server.description
+        _server.cod_info = server.codInfo
+        _server.version = server.shortversion
+        _server.last_seen = new Date()
+        _server.country_code = server.country
+        _server.country = server.countryDisplay
+
+        // add server to database
+        await db.updateOrInsertServer(_server)
+    }
+
+    servers.xlabs = _servers
 })
 
 
