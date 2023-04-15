a5b3d4f6d825af5159a4cd9c5d7d9a49eaa9f5ae
mxve <68632137+mxve@users.noreply.github.com>
2023-04-15 17:11:25 +0200

    [boiii] add country

--- api/platforms/boiii.js
+++ api/platforms/boiii.js
@@ -1,8 +1,10 @@
+const geoip = require('geoip-lite')
 const DPMaster = require('../dpmaster.js');
 const misc = require('../misc.js');
 const names = require('../names2.js');
 
 let master;
+const country_name = new Intl.DisplayNames(['en'], { type: 'region' });
 
 async function main() {
     master = new DPMaster({host: 'master.xlabs.dev', port: 20810}, 5, 30, 'T7', await misc.getBoiiiProtocol());
@@ -13,13 +15,11 @@ async function getServers() {
     let servers = await master.getServers();
     let boiii_servers = [];
     for (const server of servers.values()) {
-        // if server.info.challenge is undefined, the server is not responding
         if (!server.info) {
             return;
         }
 
         let info = server.info;
-        // remove info key from server object
         delete server.info;
         info = {
             ...server,
@@ -48,7 +48,16 @@ async function getServers() {
             dedicated: misc.string_number_to_bool(info.dedicated),
             shortversion: info.shortversion,
             known: true,
+            changed: true,
             platform: 'boiii',
+        }
+
+        try {
+            info.country = geoip.lookup(info.ip).country.toLowerCase()
+            info.countryDisplay = country_name.of(info.country.toUpperCase())
+        } catch {
+            info.country = 'lgbt'
+            info.countryDisplay = 'Unknown'
         }
         boiii_servers.push(info);
     }
