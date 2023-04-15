c6429f8e8ded75d3a51e8ec12fba2a1dd19afcd5
mxve <68632137+mxve@users.noreply.github.com>
2023-04-15 17:06:10 +0200

    add boiii to api

--- api/app.js
+++ api/app.js
@@ -5,6 +5,7 @@ const { ToadScheduler, SimpleIntervalJob, Task } = require('toad-scheduler')
 
 const plutonium = require('./platforms/plutonium.js')
 const xlabs = require('./platforms/xlabs.js')
+const boiii = require('./platforms/boiii.js')
 
 const scheduler = new ToadScheduler()
 
@@ -20,18 +21,23 @@ let servers = {
             t6zm: [],
             all: []
         },
-        date:  0
+        date: 0
     },
     xlabs: {
         servers: {
-                iw4x: [],
-                iw6x: [],
-                s1x: [],
-                boiii: [],
-                all: []
+            iw4x: [],
+            iw6x: [],
+            s1x: [],
+            all: []
         },
-        date:  0
+        date: 0
     },
+    boiii: {
+        servers: {
+            all: []
+        },
+        date: 0
+    }
 }
 
 const get_plutonium_servers = new Task('get_plutonium_servers', async () => {
@@ -40,6 +46,13 @@ const get_plutonium_servers = new Task('get_plutonium_servers', async () => {
 const get_xlabs_servers = new Task('get_xlabs_servers', async () => {
     servers.xlabs = await xlabs.getServers()
 })
+const get_boiii_servers = new Task('get_boiii_servers', async () => {
+    const _servers = await boiii.getServers()
+    if (_servers) {
+        servers.boiii = _servers
+    }
+})
+
 
 
 const get_plutonium_servers_job = new SimpleIntervalJob({ seconds: 20 }, get_plutonium_servers)
@@ -50,6 +63,10 @@ const get_xlabs_servers_job = new SimpleIntervalJob({ seconds: 20 }, get_xlabs_s
 scheduler.addSimpleIntervalJob(get_xlabs_servers_job)
 get_xlabs_servers.execute()
 
+const get_boiii_servers_job = new SimpleIntervalJob({ seconds: 1 }, get_boiii_servers)
+scheduler.addSimpleIntervalJob(get_boiii_servers_job)
+
+
 function appendServerStats(servers) {
     let countPlayers = 0
     let countBots = 0
@@ -57,14 +74,16 @@ function appendServerStats(servers) {
 
     for (server of servers) {
         if (
-          (server.platform == 'plutonium' && server.game !== 't4mp') ||
-          (server.game !== 'iw4x' && server.platform == 'xlabs')
-        ){
+            (server.platform == 'plutonium' && server.game !== 't4mp') ||
+            (server.game !== 'iw4x' && server.platform == 'xlabs')
+        ) {
             server.realClients = server.players.length - server.bots
         } else if (server.game == 'iw4x') {
             server.realClients = server.clients
         } else if (server.game == 't4mp') {
             server.realClients = server.players.length
+        } else if (server.platform == 'boiii') {
+            server.realClients = server.clients - server.bots
         }
 
         countPlayers += server.realClients
@@ -86,16 +105,17 @@ app.disable("x-powered-by")
 
 app.get(['/v1/servers', '/v1/servers/all'], (req, res) => {
     try {
-        let ans = appendServerStats([...servers.xlabs.servers.all, ...servers.plutonium.servers.all])
+        let ans = appendServerStats([...servers.xlabs.servers.all, ...servers.plutonium.servers.all, ...servers.boiii.servers.all])
         ans.servers = ans.servers.sort((a, b) => {
             return (b.realClients) - (a.realClients)
         })
 
         // average date because why not
-        ans.date = (servers.xlabs.date + servers.plutonium.date) / 2
+        ans.date = (servers.xlabs.date + servers.plutonium.date + servers.boiii.date) / 3
         ans.platform = 'all'
         res.send(ans)
-    } catch {
+    } catch (err) {
+        console.log(err)
         res.send([])
     }
 })
@@ -106,7 +126,8 @@ app.get('/v1/servers/:platform', (req, res) => {
         ans.date = servers[req.params.platform].date
         ans.platform = req.params.platform
         res.send(ans)
-    } catch {
+    } catch (err) {
+        console.log(err)
         res.send([])
     }
 })
